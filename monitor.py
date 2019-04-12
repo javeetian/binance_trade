f8119171967f37272dcb7cf2b97bd9f25079f389
tianjianwei <jianweitian@qq.com>
2019-04-12 19:17:51 +0800

    trade more pairs

--- monitor.py
+++ monitor.py
@@ -69,13 +69,33 @@ def store_sell_count(count):
     transaction.commit()
     db.close()
 
-def get_balances(client):
+def get_price(client, sym):
+    result = client.get_symbol_ticker(symbol=sym)
+    return result['price']
+
+def get_availalbe_quantity(res, sym):
+    # find asset balance in list of balances
+    if "balances" in res:
+        for bal in res['balances']:
+            if bal['asset'].lower() == sym.lower():
+                return float(bal['free'])
+    return 0
+
+def get_availalbe_quantity(res, sym):
+    # find asset balance in list of balances
+    if "balances" in res:
+        for bal in res['balances']:
+            if bal['asset'].lower() == sym.lower():
+                return float(bal['free'])
+    return 0
+
+def get_balances(client, sym):
     ret = 0
     ask_quantity = 0
-    bid_quantity = 0
+    bid_quantity = 20
     try:
-        eos = client.get_asset_balance('EOS')
-        bnb = client.get_asset_balance('BNB')
+        res = client.get_account()
+        prices = client.get_symbol_ticker()
     except requests.exceptions.ConnectionError as e:  # This is the correct syntax
         logging.error(e)
         ret = -1
@@ -83,23 +103,23 @@ def get_balances(client):
         logging.error(e)
         ret = -2
     else:
-        ask_quantity = float(eos['free']) * 0.2
-        if(ask_quantity < 20):
+        print res
+        print prices
+        if(ask_quantity > 1000):
             ask_quantity = 0
             notify("Notify", 'Not enough EOS')
-        bid_quantity = float(bnb['free']) * 0.2
-        if(bid_quantity < 10):
+        if(bid_quantity > get_availalbe_quantity(res,'BNB')):
             bid_quantity = 0
             notify("Notify", 'Not enough BNB')
         logging.debug('Balances EOS: ' + str(eos['free']) + ' BNB: ' + str(bnb['free']))
     return ret, round(ask_quantity, 2), round(bid_quantity, 2)
 
-def get_bids_asks(client):
+def get_bids_asks(client, sym):
     ret = 0
     bids3 = 0
     asks3 = 0
     try:
-        depth = client.get_order_book(symbol='EOSBNB')
+        depth = client.get_order_book(symbol=sym)
     except requests.exceptions.ConnectionError as e:  # This is the correct syntax
         logging.error(e)
         ret = -1
@@ -117,10 +137,10 @@ def get_bids_asks(client):
                 asks3 = [asks[0], asks[1], asks[2]]
     return ret, bids3, asks3
 
-def check_open_orders(client):
+def check_open_orders(client, sym):
     ret = 0
     try:
-        orders = client.get_open_orders(symbol='EOSBNB')
+        orders = client.get_open_orders()
     except requests.exceptions.ConnectionError as e:  # This is the correct syntax
         logging.error(e)
         ret = -1
@@ -135,9 +155,9 @@ def check_open_orders(client):
         for order in orders:
             notify_str = 'have open orders, please check, now exit program!!!!'
             notify("Notify", notify_str)
-            mqttc.publish(PUB_TOPIC,payload=notify_str,qos=0)
+            #mqttc.publish(PUB_TOPIC,payload=notify_str,qos=0)
             exit(1)
-            #client.cancel_order(symbol='EOSBNB', orderId=order['orderId'])
+            #client.cancel_order(symbol=sym, orderId=order['orderId'])
     return ret
 
 
@@ -150,11 +170,12 @@ client = Client(api_key, api_secret)
 profit = 0.06
 sleep_time = 50
 PUB_TOPIC = 'tjwtjwtjw'
-mqttc = mqtt.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-mqttc.connect("test.mosquitto.org")
-mqttc.loop_start()
+trade_pairs = ['EOSBNB','LTCBNB','ADABNB','XRPBNB','TRXBNB','FETBNB','BTTBNB','RVNBNB', 'CELRBNB', 'NEOBNB', 'NANOBNB', 'IOSTBNB', 'THETABNB', 'ONTBNB', 'ENJBNB', 'XLMBNB']
+#mqttc = mqtt.Client()
+#mqttc.on_connect = on_connect
+#mqttc.on_message = on_message
+#mqttc.connect("test.mosquitto.org")
+#mqttc.loop_start()
 
 
 while (1):
@@ -164,21 +185,23 @@ while (1):
     sell_price = last_price * (1 + profit)
     buy_price = last_price * (1 - profit)
     logging.info('buy price: ' + str(buy_price) + 'sell price:' + str(sell_price))
-    ret = check_open_orders(client)
+    ret = check_open_orders(client, trade_pairs)
     if ret <  0:
         time.sleep(10)
         continue
-    ret, order_ask_quantity, order_bid_quantity = get_balances(client)
+    ret, order_ask_quantity, order_bid_quantity = get_balances(client, trade_pairs)
     if ret <  0:
         time.sleep(10)
         continue
     logging.info('buy quantity: ' + str(order_bid_quantity) + 'sell quantity: ' + str(order_ask_quantity))
-    ret, bids3, asks3 = get_bids_asks(client)
+    ret, bids3, asks3 = get_bids_asks(client, trade_pairs)
     if ret <  0:
         time.sleep(10)
         continue
     logging.info('bids: ' + str(bids3))
     logging.info('asks: ' + str(asks3))
+    time.sleep(sleep_time)
+    continue
     #sell
     if(sell_price < float(bids3[2][0])):
         if((order_ask_quantity > 0) and (order_ask_quantity < (float(bids3[0][1]) + float(bids3[1][1]) + float(bids3[2][1])))):
@@ -188,10 +211,10 @@ while (1):
             store_price(last_price)
             sell_count += 1
             store_sell_count(sell_count)
-            response = client.create_order(symbol='EOSBNB', side='SELL', type='LIMIT', quantity=order_ask_quantity, price=float(bids3[2][0]), timeInForce='GTC')
+            response = client.create_order(symbol=trade_pairs, side='SELL', type='LIMIT', quantity=order_ask_quantity, price=float(bids3[2][0]), timeInForce='GTC')
             logging.warn(response)
             notify("Notify", notify_str)
-            mqttc.publish(PUB_TOPIC,payload=notify_str,qos=0)
+            #mqttc.publish(PUB_TOPIC,payload=notify_str,qos=0)
         else:
             if(order_ask_quantity > 0):
                 sleep_time = 5
@@ -204,10 +227,10 @@ while (1):
             store_price(last_price)
             sell_count -= 1
             store_sell_count(sell_count)
-            response = client.create_order(symbol='EOSBNB', side='BUY', type='LIMIT', quantity=order_bid_quantity, price=float(asks3[2][0]), timeInForce='GTC')
+            response = client.create_order(symbol=trade_pairs, side='BUY', type='LIMIT', quantity=order_bid_quantity, price=float(asks3[2][0]), timeInForce='GTC')
             logging.warn(response)
             notify("Notify", notify_str)
-            mqttc.publish(PUB_TOPIC,payload=notify_str,qos=0)
+            #mqttc.publish(PUB_TOPIC,payload=notify_str,qos=0)
         else:
             if(order_ask_quantity > 0):
                 sleep_time = 5
@@ -215,6 +238,6 @@ while (1):
     if(sell_count > 3 or sell_count < -3):
         logging.warn('abnormal sell_count!!!!!!')
         notify("Warning", "abnormal sell_count!!!!!!")
-        mqttc.publish(PUB_TOPIC,payload='abnormal sell_count!!!!!!',qos=0)
+        #mqttc.publish(PUB_TOPIC,payload='abnormal sell_count!!!!!!',qos=0)
     time.sleep(sleep_time)
 
