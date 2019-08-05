ccf4f344ae571290f311c2340a95829d19a627fd
tjw <jianweitian@qq.com>
2019-08-05 23:15:50 +0800

    update conf file

--- monitor.bat
+++ monitor.bat
@@ -2,7 +2,11 @@
 
 :loop
 
-call monitor.exe
+call monitor.exe api_key api_secret bot_token bot_chatID monitor_bnb.ini
+
+ping -n 30 127.0.0.1>nul
+
+call monitor.exe api_key api_secret bot_token bot_chatID monitor_btc.ini
 
 ping -n 300 127.0.0.1>nul
 
